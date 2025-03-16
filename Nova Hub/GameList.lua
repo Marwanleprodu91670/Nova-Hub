8faa695b284fd609379949bf1a22456d745fc8a4
Encrypted <122436701+Marwanleprodu91670@users.noreply.github.com>
2025-03-16 01:50:13 +0100

    Update and rename GameList to GameList.lua

---
+++ Nova Hub/GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [3623096087] = "https://raw.githubusercontent.com/Marwanleprodu91670/Games/refs/heads/main/Muscle%20Legends", -- Muscle Legends
+  [3101667897] = "https://raw.githubusercontent.com/Marwanleprodu91670/Games/refs/heads/main/Legends%20Of%20Speed", -- Legends Of Speed
+}
+
+return Games
